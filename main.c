a2d7f5bf2576c644684c0e246560cac7292d343b
Ivan <ivan@LaPatataDeIvan2>
2025-01-25 15:27:44 +0100

    h

--- main.c
+++ main.c
@@ -12,6 +12,16 @@
 
 #include "pipex.h"
 
+void	free_split(char **arr)
+{
+	int	i;
+
+	i = 0;
+	while (arr[i])
+		free(arr[i++]);
+	free(arr);
+}
+
 char	*find_path(char *cmd, char **envp)
 {
 	char	**env;
@@ -33,6 +43,8 @@ char	*find_path(char *cmd, char **envp)
 					return (path);
 				j++;
 			}
+			if (env)
+				free_split(env);
 		}
 		i++;
 	}
@@ -43,6 +55,7 @@ int	child1(char **argv, char **envp, int pipefd[2], int infile)
 {
 	char	*cmd;
 	int		pid;
+	char	**cmd_split;
 
 	pid = fork();
 	if (pid < 0)
@@ -53,12 +66,16 @@ int	child1(char **argv, char **envp, int pipefd[2], int infile)
 	if (pid == 0)
 	{
 		close(pipefd[0]);
-		cmd = find_path(*ft_split(argv[2], ' '), envp);
+		cmd_split = ft_split(argv[2], ' ');
+		cmd = find_path(*cmd_split, envp);
 		dup2(infile, STDIN_FILENO);
 		dup2(pipefd[1], STDOUT_FILENO);
 		close(pipefd[1]);
-		execve(cmd, ft_split(argv[2], ' '), envp);
+		execve(cmd, cmd_split, envp);
+		free(cmd);
+		free_split(cmd_split);
 		perror("execve failed");
+		exit(EXIT_FAILURE);
 	}
 }
 
@@ -66,6 +83,7 @@ int	child2(char **argv, char **envp, int pipefd[2], int outfile)
 {
 	char	*cmd;
 	int		pid;
+	char	**cmd_split;
 
 	pid = fork();
 	if (pid < 0)
@@ -76,13 +94,25 @@ int	child2(char **argv, char **envp, int pipefd[2], int outfile)
 	if (pid == 0)
 	{
 		close(pipefd[1]);
-		cmd = find_path(*ft_split(argv[3], ' '), envp);
+		cmd_split = ft_split(argv[3], ' ');
+		cmd = find_path(*cmd_split, envp);
 		dup2(pipefd[0], STDIN_FILENO);
 		close(pipefd[0]);
 		dup2(outfile, STDOUT_FILENO);
-		execve(cmd, ft_split(argv[3], ' '), envp);
+		execve(cmd, cmd_split, envp);
+		free(cmd);
+		free_split(cmd_split);
 		perror("execve failed");
+		exit(EXIT_FAILURE);
 	}
+}
+
+void	close_files(int *pipefd, int infile, int outfile)
+{
+	close(pipefd[0]);
+	close(pipefd[1]);
+	close(infile);
+	close(outfile);
 }
 
 int	main(int argc, char **argv, char **envp)
@@ -95,14 +125,16 @@ int	main(int argc, char **argv, char **envp)
 	{
 		infile = open(argv[1], O_RDONLY);
 		outfile = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-		pipe(pipefd);
+		if (infile < 0 || outfile < 0)
+			return (1);
+		if (pipe(pipefd) < 0)
+			return (1);
 		if (child1(argv, envp, pipefd, infile) == 1)
 			return (1);
 		if (child2(argv, envp, pipefd, outfile) == 1)
 			return (1);
 	}
-	close(pipefd[0]);
-	close(pipefd[1]);
+	close_files(pipefd, infile, outfile);
 	wait(NULL);
 	wait(NULL);
 	return (0);
